310de3b45ec4b94a946acb85ce4eae7bbeb628a1
saadi001 <sheikhsadi1620@gmail.com>
2023-08-27 20:58:41 +0600

    projects page completed

---
+++ src/pages/articles.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React from "react";
+
+const Article = () => {
+  return (
+    <>
+      <Head>
+        <title>About | sekh saadi</title>
+        <meta name="description" content="any description"></meta>
+      </Head>
+      <meta></meta>
+    </>
+  );
+};
+
+export default Articl;
---
+++ src/pages/projects.js
@@ -0,0 +1,166 @@
+import AnimatedText from "@/component/AnimatedText";
+import LayoutPage from "@/component/Layout";
+import { GithubIcon } from "@/component/icons";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+
+const FeaturedProduct = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full flex justify-between items-center rounded-br-2xl rounded-3xl border border-solid border-dark bg-light shadow-2x p-12 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-2xl"></div>
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto"></Image>
+      </Link>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+        <p className="font-medium my-2 text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} className="w-10">
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const ProjectComponent = ({ title, type, img, link, github }) => {
+  return (
+    <>
+      <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-2xl"></div>
+        <Link
+          href={link}
+          target="_blank"
+          className="w-full cursor-pointer overflow-hidden rounded-lg"
+        >
+          <Image src={img} alt={title} className="w-full h-auto"></Image>
+        </Link>
+
+        <div className="w-full flex flex-col items-start justify-between mt-4">
+          <span className="text-primary font-medium text-xl text-left">
+            {type}
+          </span>
+          <Link
+            href={link}
+            target="_blank"
+            className="hover:underline underline-offset-2"
+          >
+            <h2 className="my-2 w-full text-left text-3xl font-bold">
+              {title}
+            </h2>
+          </Link>
+          <div className="w-full mt-2 flex items-center justify-between">
+            <Link
+              href={link}
+              target="_blank"
+              className="text-lg font-semibold underline"
+            >
+              Visit
+            </Link>
+            <Link href={github} target="_blank" className="w-8">
+              <GithubIcon />
+            </Link>
+          </div>
+        </div>
+      </article>
+    </>
+  );
+};
+
+const Project = () => {
+  return (
+    <>
+      <Head>
+        <title>Projects | sekh saadi</title>
+        <meta name="project of sekh saadi" content="any description"></meta>
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <LayoutPage className="pt-16 mt-1">
+          <AnimatedText
+            text={"Imagination trumps knowledge!"}
+            className="mb-16"
+          />
+
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
+            <div className="col-span-12">
+              <FeaturedProduct
+                title={"Crypto Screener Application"}
+                img={project1}
+                link={"/"}
+                summary={
+                  "A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency. "
+                }
+                github={"/"}
+                type={"Featured project"}
+              />
+            </div>
+            <div className="col-span-6">
+              <ProjectComponent
+                title={"Crypto Screener Application"}
+                img={project1}
+                link={"/"}
+                github={"/"}
+                type={"project"}
+              />
+            </div>
+            <div className="col-span-6"><ProjectComponent
+                title={"Crypto Screener Application"}
+                img={project1}
+                link={"/"}
+                github={"/"}
+                type={"project"}
+              /></div>
+            <div className="col-span-12"><FeaturedProduct
+                title={"Crypto Screener Application"}
+                img={project1}
+                link={"/"}
+                summary={
+                  "A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency. "
+                }
+                github={"/"}
+                type={"Featured project"}
+              /></div>
+            <div className="col-span-6"><ProjectComponent
+                title={"Crypto Screener Application"}
+                img={project1}
+                link={"/"}
+                github={"/"}
+                type={"project"}
+              /></div>
+            <div className="col-span-6"><ProjectComponent
+                title={"Crypto Screener Application"}
+                img={project1}
+                link={"/"}
+                github={"/"}
+                type={"project"}
+              /></div>
+          </div>
+        </LayoutPage>
+      </main>
+    </>
+  );
+};
+
+export default Project;
