1bfe3d02cb164a59909d708de6258c30084e9609
saadi001 <sheikhsadi1620@gmail.com>
2023-08-30 06:40:51 +0600

    article page completed

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -3,16 +3,71 @@ import LayoutPage from "@/component/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 import article1 from "../../public/images/articles/pagination component in reactjs.jpg";
 import article2 from "../../public/images/articles/create loading screen in react js.jpg";
-import { motion } from "framer-motion";
+import article3 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
+import article4 from "../../public/images/articles/smooth scrolling in reactjs.png";
+import { motion, useMotionValue } from "framer-motion";
 
 const FramerImage = motion(Image);
 
+const MovingImage = ({ img, title, link }) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  function handleMouse(event) {
+    imgRef.current.style.display = "inline-block";
+    x.set(event.pageX);
+    y.set(-10);
+  }
+  function handleMouseLeave(event) {
+    imgRef.current.style.display = "none";
+    x.set(0);
+    y.set(0);
+  }
+
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      onMouseMove={handleMouse}
+      onMouseLeave={handleMouseLeave}
+    >
+      <h2 className="capitalize text-xl font-semibold hover:underline">
+        {title}
+      </h2>
+      <FramerImage
+        style={{ x: x, y: y }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, transition: { duration: 0.2 } }}
+        ref={imgRef}
+        src={img}
+        alt={title}
+        className="w-96 z-10 h-auto hidden absolute rounded-lg"
+      ></FramerImage>
+    </Link>
+  );
+};
+
+const ArticleComponent = ({ img, title, date, link }) => {
+  return (
+    <motion.li 
+    initial={{y:200}}
+    whileInView={{y:0, transition: {duration: 0.5, ease: "easeInOut"}}}
+    viewport={{once: true}}
+    className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 last:mb-32 border border-solid border-dark border-r-4 border-b-4">
+      <MovingImage img={img} title={title} link={link} />
+      <span className="font-semibold text-primary pl-4">{date}</span>
+    </motion.li>
+  );
+};
+
 const FeatureArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className="col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
+    <li className="col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-2xl"></div>
       <Link
         href={link}
         target="_blank"
@@ -47,7 +102,7 @@ const Article = () => {
       <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
         <LayoutPage className="pt-16 mt-1">
           <AnimatedText
-            text={"Words Can Change The World! "}
+            text={"Words Can Change The World!"}
             className="mb-16 "
           />
           <ul className="grid grid-cols-2 gap-16">
@@ -75,6 +130,42 @@ const Article = () => {
               img={article2}
             />
           </ul>
+
+          <h2 className="font-bold text-4xl w-full text-center my-16 mt-32">
+            Articles
+          </h2>
+          <ArticleComponent
+            title={
+              "Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            }
+            img={article3}
+            date={"Sept 2023"}
+            link={"/"}
+          />
+          <ArticleComponent
+            title={
+              "Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            }
+            img={article4}
+            date={"Sept 2023"}
+            link={"/"}
+          />
+          <ArticleComponent
+            title={
+              "Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            }
+            img={article3}
+            date={"Sept 2023"}
+            link={"/"}
+          />
+          <ArticleComponent
+            title={
+              "Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            }
+            img={article3}
+            date={"Sept 2023"}
+            link={"/"}
+          />
         </LayoutPage>
       </main>
     </>
