13a5167b78ebe0d7169d3c222128fa6411d35e35
saadi001 <sheikhsadi1620@gmail.com>
2023-09-01 02:13:53 +0600

    navbar responsive updated

--- src/component/Logo.js
+++ src/component/Logo.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const Logo = () => {
   return (
-    <motion.div className="flex justify-center items-center mt-2 bg-dark text-light rounded-full border border-solid border-transparent dark:border-light z-50"
+    <motion.div className="flex justify-center items-center mt-2 bg-dark text-light rounded-full border border-solid border-transparent dark:border-light "
     whileHover={{
         backgroundColor: [
           "#121212",
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -40,6 +40,29 @@ const NavbarPage = () => {
       </Link>
     );
   };
+
+  const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+    const handleClick = () =>{
+      toggle();
+      router.push(href)
+    }
+    return (
+      <button
+        onClick={handleClick}
+        className={`${className} relative group my-2 last:mb-2`}
+      >
+        {title}
+        <span
+          className={`h-[2px] inline-block bg-light dark:bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300  ${
+            router.asPath === href ? "w-full" : "w-0"
+          } `}
+        >
+          &nbsp;
+        </span>
+      </button>
+    );
+  };
+
   return (
     <header className="w-full px-32 py-6 font-medium flex justify-between items-center">
       <button
@@ -129,7 +152,81 @@ const NavbarPage = () => {
           </button>
         </nav>
       </div>
-      <div className="absolute left-[50%] top-1 translate-x-[-50%] z-50">
+      
+
+      {/* mobileMenu */}
+      {
+        isOpen ? 
+        <div className="min-w-[70vw] flex-col z-30 justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/90 rounded-lg backdrop-blur-md py-32 text-light dark:text-dark hidden lg:flex">
+        <nav className="flex flex-col items-center justify-center">
+          <CustomMobileLink href={"/"} title={"Home"} className=" " toggle={handleClick}/>
+          <CustomMobileLink href={"/about"} title={"About"} className="" toggle={handleClick}/>
+          <CustomMobileLink href={"/projects"} title={"Projects"} className="" toggle={handleClick}/>
+          <CustomMobileLink href={"/articles"} title={"Articles"} className="" toggle={handleClick}/>
+        </nav>
+
+        <nav className="flex justify-center items-center flex-wrap mt-2">
+          <motion.a
+            href={"/"}
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mr-3 sm:mx-1"
+          >
+            <TwitterIcon />
+          </motion.a>
+          <motion.a
+            href={"/"}
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3 sm:mx-1"
+          >
+            <GithubIcon className={"dark:text-light"} />
+          </motion.a>
+          <motion.a
+            href={"/"}
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3 sm:mx-1"
+          >
+            <LinkedInIcon />
+          </motion.a>
+          <motion.a
+            href={"/"}
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3 sm:mx-1"
+          >
+            <PinterestIcon />
+          </motion.a>
+          <motion.a
+            href={"/"}
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3 sm:mx-1"
+          >
+            <DribbbleIcon></DribbbleIcon>
+          </motion.a>
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className="ml-3 sm:ml-1 flex items-center justify-center rounded-full p-1 bg-dark text-light dark:bg-light dark:text-dark"
+          >
+            {mode === "dark" ? (
+              <SunIcon className={"fill-dark"} />
+            ) : (
+              <MoonIcon className={"fill-dark"} />
+            )}
+          </button>
+        </nav>
+      </div>
+        : null
+      }
+
+      <div className="absolute left-[50%] top-1 translate-x-[-50%] z-20">
         <Logo></Logo>
       </div>
     </header>
